c6083e98b9b8bb052f4ab9958a244cd5495d29b3
larissalima2006 <106171712+larissalima2006@users.noreply.github.com>
2022-11-16 12:40:21 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -1,1 +1,27 @@
 
+var titulo=document.querSelector(".titulo");
+titulo.textContent="Aparecida Nutricionistas";
+
+var pacientes= document.querSelectorrAll(".paciente");
+
+for(var i=0;< pacientes.length; i++){
+
+	var paciente = pacientes[i];
+
+	var tdPeso = paciente. querySeletor(".info-peso");
+	var peso = tdPeso.textContent;
+
+	var tdAltura = paciente.querySelector("info- altura");
+	var altura = tdAltura.textContent;
+
+	var tdAltura = paciente.querySelector(".info-imc");
+
+ 	var pesoEhValido = true;
+ 	var alturaehValido = true;
+
+ 	if(peso <= 0 ||>=1000){
+ 		console.log("Peso inválido!");
+ 		pesoEhValido = false;
+ 		td
+ 	}
+}
